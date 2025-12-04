cf71b1791255028c0522380fafcf63f39a245587
Masiha Majidzadeh <hashpragmaonce@gmail.com>
2025-12-04 18:25:16 +0330

    Create a new project and prepare a simple main activity UI layout

---
+++ MaterialsLiveWallpaper2/app/src/main/java/com/reminimalism/materialslivewallpaper2/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.reminimalism.materialslivewallpaper2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.reminimalism.materialslivewallpaper2.ui.theme.MaterialsLiveWallpaper2Theme
+
+class MainActivity : ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialsLiveWallpaper2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier)
+{
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center)
+    {
+        Column(modifier.padding(10.dp, 10.dp), horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Row(verticalAlignment = Alignment.CenterVertically)
+            {
+                Text(
+                    text = "Materials",
+                    fontSize = 30.sp,
+                    color = Color(0x00, 0xDC, 0xC8)
+                )
+                Text(
+                    text = "Live",
+                    fontSize = 30.sp,
+                    color = Color(0xFF, 0x00, 0x64)
+                )
+                Text(
+                    text = "Wallpaper",
+                    fontSize = 30.sp,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Text(
+                    text = "2",
+                    fontSize = 30.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(
+                onClick = {},
+                content = {
+                    Text(
+                        text = "Set Wallpaper"
+                    )
+                }
+            )
+
+            Button(
+                onClick = {},
+                content = {
+                    Text(
+                        text = "Settings"
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview()
+{
+    MaterialsLiveWallpaper2Theme {
+        Greeting()
+    }
+}
